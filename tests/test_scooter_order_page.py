f3351c1135588269cde58563498e75b20ee0ad5d
Peter Gaas <gaaspetr@gmail.com>
2024-05-16 22:05:36 +0700

    add test class in test pages

--- tests/test_scooter_order_page.py
+++ tests/test_scooter_order_page.py
@@ -6,40 +6,42 @@ from pages.scooter_home_page import ScooterHomePage
 from pages.scooter_order_page import ScooterOrderPage
 
 
-@allure.title('Заказать самокат через кнопку "Заказать" вверху страницы')
-@allure.description('Проверка позитивного сценария заказа самоката с случайным наборам данных')
-@pytest.mark.parametrize(
-    'name, surname, address, metro_station, phone_number, date_calendar, rental_period, color, comment', [
-        [generate_name(), generate_surname(), generate_address(), LIST_SUBWAY_STATIONS, generate_phone_number(),
-         DATE_CALENDAR, RENTAL_PERIOD, COLLOR_SCOOTER, generate_comment()]])
-def test_button_order_top(firefox_driver, name, surname, address, metro_station, phone_number, date_calendar,
-                          rental_period, color, comment):
-    scooter_home_page = ScooterHomePage(firefox_driver)
-    scooter_home_page.click_to_button_order_top()
+class TestScooterOrderPage:
 
-    scooter_order_page = ScooterOrderPage(firefox_driver)
-    scooter_order_page.fill_fields_for_scooter(name, surname, address, metro_station, phone_number)
-    scooter_order_page.fill_fields_lease(date_calendar, rental_period, color, comment)
-    text = scooter_order_page.get_text_order_placed()
+    @allure.title('Заказать самокат через кнопку "Заказать" вверху страницы')
+    @allure.description('Проверка позитивного сценария заказа самоката с случайным наборам данных')
+    @pytest.mark.parametrize(
+        'name, surname, address, metro_station, phone_number, date_calendar, rental_period, color, comment', [
+            [generate_name(), generate_surname(), generate_address(), LIST_SUBWAY_STATIONS, generate_phone_number(),
+             DATE_CALENDAR, RENTAL_PERIOD, COLLOR_SCOOTER, generate_comment()]])
+    def test_button_order_top(self, firefox_driver, name, surname, address, metro_station, phone_number, date_calendar,
+                              rental_period, color, comment):
+        scooter_home_page = ScooterHomePage(firefox_driver)
+        scooter_home_page.click_to_button_order_top()
 
-    assert "Заказ оформлен" in text
+        scooter_order_page = ScooterOrderPage(firefox_driver)
+        scooter_order_page.fill_fields_for_scooter(name, surname, address, metro_station, phone_number)
+        scooter_order_page.fill_fields_lease(date_calendar, rental_period, color, comment)
+        text = scooter_order_page.get_text_order_placed()
 
+        assert "Заказ оформлен" in text
 
-@allure.title('Заказать самокат через кнопку "Заказать" внизу страницы')
-@allure.description('Проверка позитивного сценария заказа самоката с случайным наборам данных')
-@pytest.mark.parametrize(
-    'name, surname, address, metro_station, phone_number, date_calendar, rental_period, color, comment', [
-        [generate_name(), generate_surname(), generate_address(), LIST_SUBWAY_STATIONS, generate_phone_number(),
-         DATE_CALENDAR, RENTAL_PERIOD, COLLOR_SCOOTER, generate_comment()]])
-def test_button_order_bottom(firefox_driver, name, surname, address, metro_station, phone_number, date_calendar,
-                             rental_period, color, comment):
-    scooter_home_page = ScooterHomePage(firefox_driver)
-    scooter_home_page.scroll_to_button_order_bottom()
-    scooter_home_page.click_to_button_order_bottom()
+    @allure.title('Заказать самокат через кнопку "Заказать" внизу страницы')
+    @allure.description('Проверка позитивного сценария заказа самоката с случайным наборам данных')
+    @pytest.mark.parametrize(
+        'name, surname, address, metro_station, phone_number, date_calendar, rental_period, color, comment', [
+            [generate_name(), generate_surname(), generate_address(), LIST_SUBWAY_STATIONS, generate_phone_number(),
+             DATE_CALENDAR, RENTAL_PERIOD, COLLOR_SCOOTER, generate_comment()]])
+    def test_button_order_bottom(self, firefox_driver, name, surname, address, metro_station, phone_number,
+                                 date_calendar,
+                                 rental_period, color, comment):
+        scooter_home_page = ScooterHomePage(firefox_driver)
+        scooter_home_page.scroll_to_button_order_bottom()
+        scooter_home_page.click_to_button_order_bottom()
 
-    scooter_order_page = ScooterOrderPage(firefox_driver)
-    scooter_order_page.fill_fields_for_scooter(name, surname, address, metro_station, phone_number)
-    scooter_order_page.fill_fields_lease(date_calendar, rental_period, color, comment)
-    text = scooter_order_page.get_text_order_placed()
+        scooter_order_page = ScooterOrderPage(firefox_driver)
+        scooter_order_page.fill_fields_for_scooter(name, surname, address, metro_station, phone_number)
+        scooter_order_page.fill_fields_lease(date_calendar, rental_period, color, comment)
+        text = scooter_order_page.get_text_order_placed()
 
-    assert "Заказ оформлен" in text
+        assert "Заказ оформлен" in text
